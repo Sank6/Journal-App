27d352f8ecb57a3ded52120ec26b4f5f999538ee
Sankarsh <sankarsh@sankarsh.home>
2021-02-13 18:00:21 +0000

    Defer install prompt

--- assets/static/js/main.js
+++ assets/static/js/main.js
@@ -7,3 +7,17 @@ function ready() {
     let bg = document.getElementById("bg");
     document.body.style.backgroundImage = `url("${bg.innerText}")`;
 }
+
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
